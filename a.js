cf6a9c75be6a309714573c3806ec1e72d78a868f
TomatoSix <1059421623@qq.com>
2022-03-22 23:24:30 +0800

    feat: 算法新增并查集

---
+++ a.js
@@ -0,0 +1,11 @@
+const animalList = ['cat']
+
+
+const showAnimal = () => {
+  console.log(animalList);
+}
+
+module.exports = {
+  animalList,
+  showAnimal
+}
---
+++ b.js
@@ -0,0 +1,9 @@
+const animalAUtil = require('./a')
+const animalBUtil = require('./a')
+
+
+animalAUtil.animalList.push('dog');
+animalBUtil.animalList.push('tiger');
+
+animalAUtil.showAnimal()
+animalBUtil.showAnimal()
