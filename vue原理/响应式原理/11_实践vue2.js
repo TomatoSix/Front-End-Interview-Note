d1046643ec7aea6354a889aeba74a0c838225079
TomatoSix <1059421623@qq.com>
2022-10-23 14:10:02 +0800

    feat: 文件更新

--- vue原理/响应式原理/11_实践vue2.js
+++ vue原理/响应式原理/11_实践vue2.js
@@ -1,86 +1,75 @@
-let activeReactiveFn = null;
+let activeReactiveFn = null
 
-// 定义函数收集的类
+
+function watchFn(fn) {
+  activeReactiveFn = fn
+  fn()
+  activeReactiveFn = null
+}
+
 class Depend {
   constructor() {
-    this.reactiveFns = new Set();
+    this.reactiveFns = new Set()
   }
 
-  // addDepend(reactiveFn) {
-  //   this.reactiveFns.push(reactiveFn)
-  // }
-
   notify() {
-    this.reactiveFns.forEach((fn) => {
-      fn();
-    });
+    this.reactiveFns.forEach(fn => fn())
   }
 
   depend() {
     if (activeReactiveFn) {
-      this.reactiveFns.add(activeReactiveFn);
+      this.reactiveFns.add(activeReactiveFn)
     }
   }
 }
-const depend = new Depend();
 
-// 封装一个收集需要调用的函数
-function watchFn(fn) {
-  // 把需要响应的函数放入正确的依赖中
-  // 1. 找到depend对象
-  activeReactiveFn = fn;
-  fn();
-  activeReactiveFn = null;
-}
-
-// 封装一个获取depend的函数
-const targetMap = new WeakMap();
+let wk = new WeakMap()
 function getDepend(target, key) {
-  // 根据target获取Map
-  let map = targetMap.get(target);
+  let map = wk.get(target)
   if (!map) {
-    // 初次使用不存在map
-    map = new Map();
-    targetMap.set(target, map);
+    map = new Map()
+    wk.set(target, map)
   }
-
-  let depend = map.get(key);
+  let depend = map.get(key)
   if (!depend) {
-    depend = new Depend();
-    map.set(key, depend);
+    depend = new Depend()
+    map.set(key, depend)
   }
-  return depend;
+  return depend
 }
+
 
 function reactive(obj) {
-  Object.keys(obj).forEach((key) => {
-    let value = obj[key];
-    Object.defineProperty(obj, key, {
-      get: function () {
-        const depend = getDepend(obj, key);
-        depend.depend();
-        return value;
-      },
-      // 只有一个属性newValue
-      set: function (newValue) {
-        value = newValue;
-        const depend = getDepend(obj, key);
-        depend.notify();
-      },
-    });
-  });
-  return obj;
+  return new Proxy(obj, {
+    get(target, key, receiver) {
+      let depend = getDepend(target, key)
+      depend.depend()
+      return Reflect.get(target, key, receiver)
+    },
+    set(target, key, value, receiver) {
+      Reflect.set(target, key, value, receiver)
+
+      let depend = getDepend(target, key)
+      depend.notify()
+    }
+  })
 }
 
-//对象的响应式
-const obj = {
-  name: "six",
-  age: 18,
-};
-let proxyObj = reactive(obj);
+
+
+
+
+
+
+var obj = {
+  name: 'six',
+  age: 18
+}
+
+const objProxy = reactive(obj)
 
 watchFn(() => {
-  console.log(proxyObj.name, "obj被修改了");
-});
+  console.log(objProxy.name, 'name被修改过');
+})
 
-proxyObj.name = "番茄炒小六";
+objProxy.name = '番茄'
